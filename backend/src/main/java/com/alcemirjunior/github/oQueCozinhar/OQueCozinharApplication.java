08c7de36eae9fb018fc90b44252862e0171ed20c
alcemirjunior <alcemirmjunior@gmail.com>
2021-03-28 20:57:35 -0300

    first commit

---
+++ backend/src/main/java/com/alcemirjunior/github/oQueCozinhar/OQueCozinharApplication.java
@@ -0,0 +1,13 @@
+package com.alcemirjunior.github.oQueCozinhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OQueCozinharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OQueCozinharApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alcemirjunior/github/oQueCozinhar/OQueCozinharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcemirjunior.github.oQueCozinhar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OQueCozinharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
